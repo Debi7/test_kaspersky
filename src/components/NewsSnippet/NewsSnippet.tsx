552a1fdd936d9e54fdd24a188c5b094fabac2484
Debi7 <debi1@mail.ru>
2025-04-16 22:44:58 +0300

    constants

--- src/components/NewsSnippet/NewsSnippet.tsx
+++ src/components/NewsSnippet/NewsSnippet.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import { Card, Typography, Tag, Avatar, Collapse } from 'antd';
-import { IData_SnippetNews } from '../types/interfaces';
-import './NewsSnippet.css';
+import { IData_SnippetNews } from '../../types/interfaces';
+import styles from './NewsSnippet.module.css';
+
 
 const { Title, Text } = Typography;
 const { Panel } = Collapse;
 
 const text = "A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.";
+
 interface Props {
   news: IData_SnippetNews;
 }
 
-const NewsSnippet: React.FC<Props> = ({ news }) => {
+export const NewsSnippet: React.FC<Props> = ({ news }) => {
   const { TI, AB, URL, DP, DOM, KW, FAV, HIGHLIGHTS } = news;
 
   return (
-    <Card hoverable className='card'>
+    <Card hoverable className={styles.card}>
       <a href={URL} target="_blank" rel="noopener noreferrer">
         {FAV && <Avatar src={FAV} alt="favicon" style={{ marginBottom: '8px' }} />}
-        <Title level={4} className='link'>{TI}</Title>
+        <Title level={4} className={styles.link}>{TI}</Title>
         <Text>{new Date(DP).toLocaleString()}</Text>
         <Text type="secondary" style={{ display: 'block', marginBottom: '8px' }}>
           {DOM}
@@ -43,12 +45,10 @@ const NewsSnippet: React.FC<Props> = ({ news }) => {
       )}
 
       <Collapse style={{ marginTop: '20px' }}>
-        <Panel className='collapse-header' header="View Duplicates" key="1">
-          <p className='text'>{text}</p>
+        <Panel className={styles.collapseHeader} header="View Duplicates" key="1">
+          <p className={styles.text}>{text}</p>
         </Panel>
       </Collapse>
     </Card >
   );
 };
-
-export default NewsSnippet;
